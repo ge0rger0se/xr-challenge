04998cd15584f7a7cdaed4091b12bafb2c1c5e79
ge0rger0se <george.rose@live.co.uk>
2023-01-19 19:53:55 +0000

    Player movement finish

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float playerSpeed = 3.0f;
-
+    public float playerSpeed = 3.5f;
+    public float playerJumpHeight = 225.0f;
     public Rigidbody rb;
-
     Vector3 playerMovement;
     
     // Start is called before the first frame update
@@ -19,8 +18,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerMovement.x = Input.GetAxisRaw("Horizontal");
-        playerMovement.z = Input.GetAxisRaw("Vertical");
+        playerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space) && rb.position.y == 0.5)
+        {
+            rb.AddForce(Vector3.up * playerJumpHeight);
+        }
     }
 
     void FixedUpdate()
